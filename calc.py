e8ad176addd97480e79a73d69b4c52b4e1be8197
Gema Gonzalez Mazarias <gegonmaz@gamma24.aulas.gsyc.urjc.es>
2016-09-19 10:40:48 +0200

    añadido multiplicacion y division, falta dividir por cero

--- calc.py
+++ calc.py
@@ -15,13 +15,13 @@ def minus(op1, op2):
 
 
 def multiplicar(op1, op2):
-	return op1 * op2
+    return op1 * op2
 
 def dividir(op1, op2):
-	return op1 / op2
+    return op1 / op2
 
 if __name__ == "__main__":
-	
+    
     try: # nos aseguramos que los argumentos que le pasamos sean solo enteros, para no tener luego problemas como por ejemplo del tipo 3 suma a
         operando1 = int(sys.argv[1]) # argumento que le pasa el sistema Posicion 0 nombre programa, pos. 1 1er argumento, pos 2 suma, pos. 3 segundo argumento.
         operando2 = int(sys.argv[3])
@@ -32,10 +32,10 @@ if __name__ == "__main__":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
-	elif sys.argv[2] == "por":
-		result= multiplicar(operando1, operando2)
-	elif sys.argv[2] == "dividido":
-		result= dividir(operando1, operando2)
+    elif sys.argv[2] == "por":
+        result= multiplicar(operando1, operando2)
+    elif sys.argv[2] == "dividido":
+        result= dividir(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser sumar, restar, por o dividido.')
 
