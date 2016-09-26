1f40d413ed0c8ba766b1afb5c9aa5634ea7222fd
Gema Gonzalez Mazarias <gegonmaz@gamma16.aulas.gsyc.urjc.es>
2016-09-26 18:45:46 +0200

    pep8 pasado, todo correcto

--- calcplus.py
+++ calcplus.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys  #Cuando importamos funcionalidad de otro usuario 
-                    #-+-+ funciones para interactuar-+-
-
+import sys
 import calcoohija
 import csv
 
 if __name__ == "__main__":
 
-    archivo = open('operaciones.txt','r')
+    archivo = open('operaciones.txt', 'r')
     lista = archivo.readlines()
-    c = calcoohija.CalculadoraHija()   
+    c = calcoohija.CalculadoraHija()
 
     for linea in lista:
         operando = linea.split(',')[0]
         componentes = linea.split(',')[1:]
         result = componentes[0]
-        
         for componente in componentes[1:]:
             if operando == 'suma':
-                result = c.suma(int(result) , int(componente))
+                result = c.suma(int(result), int(componente))
             if operando == 'resta':
                 result = c.resta(int(result), int(componente))
             if operando == 'multiplica':
-                result = c.multiplicar(int(result) ,int(componente))
+                result = c.multiplicar(int(result), int(componente))
             if operando == 'divide':
-                
                 if componente == 0:
                     result = "No se puede dividir por cero"
                 else:
-                    result = c.dividir(int(result) , int(componente))
-                 
+                    result = c.dividir(int(result), int(componente))
         print(result)
-        
-        
