077f14f0e6a5a487d8ed19e3af4a9540177b7084
Gema Gonzalez Mazarias <gegonmaz@gamma16.aulas.gsyc.urjc.es>
2016-09-26 17:12:34 +0200

    Programa chequeado, ya funciona, obtenemos 15 en todos los resultado

--- calcplus.py
+++ calcplus.py
@@ -5,4 +5,33 @@ import sys  #Cuando importamos funcionalidad de otro usuario
                     #-+-+ funciones para interactuar-+-
 
 import calcoohija
+import csv
 
+if __name__ == "__main__":
+
+    archivo = open('operaciones.txt','r')
+    lista = archivo.readlines()
+    c = calcoohija.CalculadoraHija()   
+
+    for linea in lista:
+        operando = linea.split(',')[0]
+        componentes = linea.split(',')[1:]
+        result = componentes[0]
+        
+        for componente in componentes[1:]:
+            if operando == 'suma':
+                result = c.suma(int(result) , int(componente))
+            if operando == 'resta':
+                result = c.resta(int(result), int(componente))
+            if operando == 'multiplica':
+                result = c.multiplicar(int(result) ,int(componente))
+            if operando == 'divide':
+                
+                if componente == 0:
+                    result = "No se puede dividir por cero"
+                else:
+                    result = c.dividir(int(result) , int(componente))
+                 
+        print(result)
+        
+        
