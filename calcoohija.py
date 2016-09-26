fb4134c0caead60724cf2a080ede75ebe8d54ea9
Gema Gonzalez Mazarias <gegonmaz@gamma16.aulas.gsyc.urjc.es>
2016-09-26 18:44:26 +0200

    pep8 pasado, todo correcto

--- calcoohija.py
+++ calcoohija.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys  #Cuando importamos funcionalidad de otro usuario 
-                    #-+-+ funciones para interactuar-+-
-
+import sys
 import calcoo
 
+
 class CalculadoraHija(calcoo.Calculadora):
-    
-    #def __init__(self, op1, op2):
-    #Metodo de inicializacion
-   
+
     def multiplicar(self, op1, op2):
         return op1 * op2
 
     def dividir(self, op1, op2):
-        
         return op1 / op2
-
 
 if __name__ == "__main__":
     calculadorahija = CalculadoraHija()
-    try: # nos aseguramos que los argumentos que le pasamos sean solo enteros, 
-         #para no tener luego problemas como por ejemplo del tipo 3 suma a
-        operando1 = int(sys.argv[1]) # argumento que le pasa el sistema Posicion 
-                                     #0 nombre programa, pos. 1 1er argumento, 
-                                     #pos 2 suma, pos. 3 segundo argumento.
+
+    try:
+        operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
+
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
@@ -38,10 +31,9 @@ if __name__ == "__main__":
             result = calculadorahija.multiplicar(operando1, operando2)
     elif sys.argv[2] == "divide":
         if operando2 == 0:
-             pass
-        else: 
-            result = calculadorahija.dividir(operando1, operando2)        
-         
+            pass
+        else:
+            result = calculadorahija.dividir(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser suma, resta, multiplica o divide')
     print(result)
