35f597fe7752520af1128894cc851ad976576bfc
Gema Gonzalez Mazarias <gegonmaz@gamma16.aulas.gsyc.urjc.es>
2016-09-26 16:45:32 +0200

    Comienzo calculadora plus, leyendo de archivo txt

---
+++ calcplus.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys  #Cuando importamos funcionalidad de otro usuario 
+                    #-+-+ funciones para interactuar-+-
+
+import calcoohija
+
